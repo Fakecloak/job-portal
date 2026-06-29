bcadabcac13f6c474a065a168354faa15d3a3519
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-30 02:28:54 +0530

    created LoginPage component and connected the form to backend API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import RegisterPage from './components/RegisterPage'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
   return (
     <>
-    <RegisterPage />
+    {/* <RegisterPage /> */}
+    <LoginPage />
     </>
   )
 }
